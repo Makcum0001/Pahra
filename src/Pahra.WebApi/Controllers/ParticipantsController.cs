bc56dd02f124b86ff87830ba4ea549ee8838a48e
Maxim Novikov <makcnov0001@gmail.com>
2026-02-28 11:29:40 +0300

    feat(WebApi): Настроены контроллеры и сервисы API
    - Добавлен контроллер для работы с участниками.
    
    - Настроена работа с CORS, логированием и роутингом.
    
    - Добавлена документация Scalar.

---
+++ src/Pahra.WebApi/Controllers/ParticipantsController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Pahra.Application.Features.Participants.Commands;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ParticipantsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger<ParticipantsController> _logger;
+
+    public ParticipantsController(IMediator mediator, ILogger<ParticipantsController> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<int>> CreateAsync([FromBody] CreateParticipantCommand command)
+    {
+        if (command == null)
+        {
+            return BadRequest();
+        }
+
+        var id = await _mediator.Send(command);
+
+        return Created($"/api/participants/{id}", id);
+    }
+}
--- src/Pahra.WebApi/Program.cs
+++ src/Pahra.WebApi/Program.cs
@@ -1,43 +1,48 @@
+﻿using Scalar.AspNetCore;
+using Pahra.Application.Extensions;
+using Pahra.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddAuthentication()
-                .AddBearerToken();
+builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddLogging(c =>
+{
+    c.AddConsole();
+    c.AddDebug();
+});
+
+builder.Services.AddApplicationLayer();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            // В режиме разработки разрешаем всё. Для production стоит указать конкретный origin фронтенда.
+            policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseRouting();
+
+app.UseCors();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(c =>
+    {
+        c.Title = "Pahra API";
+    });
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
