8ed8eb537279b59e923c8c12f6303209c8a5db40
Maxim Novikov <makcnov0001@gmail.com>
2026-02-28 11:27:26 +0300

    feat(Domain): Начальная модель домена
    - Добавлена модель Participant и ее репозиторий.
    
    - Удалена старая сущность participant.

---
+++ src/Pahra.Domain/Repositories/IParticipantRepository.cs
@@ -0,0 +1,9 @@
+﻿using Pahra.Domain.Models;
+
+namespace Pahra.Domain.Repositories;
+
+public interface IParticipantRepository
+{
+    public Task AddAsync(Participant participant, CancellationToken cancellationToken = default);
+    public Task<List<Participant>> GetAllAsync(CancellationToken cancellationToken = default);
+}
